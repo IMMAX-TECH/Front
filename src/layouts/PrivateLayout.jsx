573c86e5d59bf86ec1a35690533e8fd999c2f121
Miguel Angel Perez Valencia <mikhaelangelx@hotmail.com>
2021-10-08 00:26:14 -0500

    agregar estilo similar a apple

--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -6,11 +6,9 @@ const PrivateLayout = ({ children }) => {
     <div className='flex w-screen h-screen'>
       <div className='flex flex-nowrap h-full w-full'>
         <Sidebar />
-<<<<<<< HEAD
-        <main className='bg-gradient-to-r from-gray-600 via-gray-400 to-gray-600 flex w-full  overflow-y-scroll items-center justify-center pr-0 '>
-=======
-        <main className='bg-gradient-to-r from-gray-800 via-gray-900 to-gray-800 flex w-full  overflow-y-scroll items-center justify-center pr-0 '>
->>>>>>> 76e45b9190920c0e5738d17abbc1ced8ce487869
+       
+        <main className='bg-gradient-to-r from-gray-700 via-gray-500 to-gray-700 flex w-full  overflow-y-scroll items-center justify-center pr-0 '>
+       
           {children}
         </main>
       </div>
